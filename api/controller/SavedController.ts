0b6c353e407f5ba5decda6c39e63ff75a5d1f868
Rafael Amaral <amaral.avelar.filo@gmail.com>
2022-09-06 00:33:35 -0300

    feat(savedcontrollers): create functions of the class

---
+++ api/controller/SavedController.ts
@@ -0,0 +1,49 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable no-underscore-dangle */
+import type { NextApiRequest, NextApiResponse } from 'next';
+import ISavedService from '../interfaces/ISavedService';
+import * as i from '../interfaces/IPokemon';
+
+export default class SavedController {
+  private _service: ISavedService;
+
+  constructor(public service: ISavedService) {
+    this._service = service;
+  }
+
+  async getAll(res: NextApiResponse) {
+    try {
+      const pokemons = await this._service.getAll();
+      res.status(200).json(pokemons);
+    } catch (e: any) {
+      res.json({ messange: e.messange });
+    }
+  }
+
+  async create(
+    res: NextApiResponse,
+    name: string,
+    pokemonId: number,
+    contactList: i.IContact[],
+    types: i.IType[],
+  ) {
+    try {
+      await this._service.create(name, pokemonId, contactList, types);
+      res.status(201).end();
+    } catch (e: any) {
+      res.json({ messange: e.messange });
+    }
+  }
+
+  async start(req: NextApiRequest, res: NextApiResponse) {
+    const { method } = req;
+    if (method === 'GET') {
+      await this.getAll(res);
+    }
+
+    if (method === 'POST') {
+      const { name, pokemonId, contactList, types } = req.body;
+      await this.create(res, name, pokemonId, contactList, types);
+    }
+  }
+}
